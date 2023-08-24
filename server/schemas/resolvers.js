801129ad22defa0a2af22e83ae3cd899ec84d318
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-23 23:39:12 -0400

    feature: added the mutations 'addGroup', 'updateGroup', and 'removeGroup' to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Group, Category, Task } = require('../models');
 const { signToken, AuthenticationError } = require('../utils');
 
 const resolvers = {
@@ -28,6 +28,54 @@ const resolvers = {
       const token = signToken(user);
 
       return { token, currentUser: user };
+    },
+    addGroup: async (parent, { groupName }, context) => {
+      if (context.user) {
+        const group = await Group.create({ groupName });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { groups: group._id } },
+          { new: true }
+        );
+        return group;
+      }
+      throw AuthenticationError('You need to be logged in!');
+    },
+    updateGroup: async (parent, { groupId, groupName }, context) => {
+      if (context.user) {
+        const groupToUpdate = await Group.findById(groupId);
+        if (!groupToUpdate) {
+          throw new Error('Group not found.');
+        }
+
+        if (!groupToUpdate.users.includes(context.user._id.toString())) {
+          throw new AuthenticationError(
+            'You are not authorized to update this group.'
+          );
+        }
+
+        groupToUpdate.groupName = groupName;
+        await groupToUpdate.save();
+
+        return groupToUpdate;
+      }
+      throw new AuthenticationError('You must be logged in to update a group.');
+    },
+    removeGroup: async (parent, { groupId }, context) => {
+      if (context.user) {
+        const group = await Group.findOneAndDelete({
+          _id: groupId,
+        });
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { groups: group._id } }
+        );
+        return group;
+      }
+      throw new AuthenticationError(
+        'You are not authorized to delete this group'
+      );
     },
   },
 };
