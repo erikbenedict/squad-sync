6675a858d8c5f0dcae25ee741779f7a754edb863
Jack <jebulger1003@gmail.com>
2023-08-24 19:22:32 -0400

    Created queries for resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,27 @@
-const { User } = require('../models');
+const { User, Group, Category, Task } = require('../models');
 const { signToken, AuthenticationError } = require('../utils');
 
 const resolvers = {
   Query: {
     currentUser: async (parent, { email }) => User.findOne({ email }),
+    group: async (parent, { groupId }) => Group.findOne({ _id: groupId }),
+    allGroups: async (parent, { userId }) => {
+      const userGroups = await User.findOne({ _id: userId }).populate('groups');
+
+      return userGroups.groups;
+    },
+    category: async (parent, { categoryId }) => Category.findOne({ _id: categoryId }),
+    allCategories: async (parent, { groupId }) => {
+      const groupCategories = await Group.findOne({ _id: groupId }).populate('categories');
+
+      return groupCategories.categories;
+    },
+    task: async (parent, { taskId }) => Task.findOne({ _id: taskId }),
+    allTasks: async (parent, { categoryId }) => {
+      const categoryTasks = await Category.findOne({ _id: categoryId }).populate('tasks');
+
+      return categoryTasks.tasks;
+    },
   },
 
   Mutation: {
