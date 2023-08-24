00594a04dd462d760b92d083aae13f409c7d00a9
Daniel S <dmschopler@gmail.com>
2023-08-24 19:53:17 -0400

    udpated queries in client side

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -11,3 +11,32 @@ export const QUERY_CURRENT_USER = gql`
     }
   }
 `;
+
+export const QUERY_USER_GROUPS = gql`
+query getUserGroups($userId: ID!) {
+  }
+`
+
+export const QUERY_GROUP_USERS = gql`
+query getGroupUsers()
+`
+
+export const QUERY_SINGLE_GROUP = gql`
+query getSingleGroup()
+`
+
+export const QUERY_GROUP_CATEGORIES = gql`
+query getGroupCategories
+`
+
+export const QUERY_SINGLE_CATEGORY = gql`
+query getSingleCategory
+`
+
+export const QUERY_CATEGORY_TASKS = gql`
+query getCategoryTasks
+`
+
+export const QUERY_SINGLE_TASK = gql`
+query getSingleTask
+`
