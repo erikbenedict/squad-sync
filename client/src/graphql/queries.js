6c5c5556da6550e2e3134c53fdf0a16310b7e73f
Daniel S <dmschopler@gmail.com>
2023-08-24 22:23:39 -0400

    finished queries on client side

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -17,30 +17,25 @@ export const QUERY_USER_GROUPS = gql`
     userGroups(userId: $userId) {
       _id
       groupName
-      categories {
-        _id
-        categoryName
-      }
       users {
         _id
         firstName
         lastName
-        email
       }
     }
   }
 `;
 
-export const QUERY_GROUP_USERS = gql`
-  query getGroupUsers($groupId: ID!) {
-    groupUsers(groupId: $groupId) {
-      _id
-      firstName
-      lastName
-      email
-    }
-  }
-`;
+// export const QUERY_GROUP_USERS = gql`
+//   query getGroupUsers($groupId: ID!) {
+//     groupUsers(groupId: $groupId) {
+//       _id
+//       firstName
+//       lastName
+//       email
+//     }
+//   }
+// `;
 
 export const QUERY_SINGLE_GROUP = gql`
   query getSingleGroup($groupId: ID!) {
@@ -55,26 +50,25 @@ export const QUERY_SINGLE_GROUP = gql`
         _id
         firstName
         lastName
-        email
       }
     }
   }
 `;
 
-export const QUERY_GROUP_CATEGORIES = gql`
-  query getGroupCategories($groupId: ID!) {
-    groupCategories(groupId: $groupId) {
-      _id
-      categoryName
-      tasks {
-        _id
-        taskName
-        taskDescription
-        dueDate
-      }
-    }
-  }
-`;
+// export const QUERY_GROUP_CATEGORIES = gql`
+//   query getGroupCategories($groupId: ID!) {
+//     groupCategories(groupId: $groupId) {
+//       _id
+//       categoryName
+//       tasks {
+//         _id
+//         taskName
+//         taskDescription
+//         dueDate
+//       }
+//     }
+//   }
+// `;
 
 export const QUERY_SINGLE_CATEGORY = gql`
   query getSingleCategory($categoryId: ID!) {
@@ -82,29 +76,32 @@ export const QUERY_SINGLE_CATEGORY = gql`
       _id
       categoryName
       tasks {
-        _id
         taskName
-        taskDescription
         dueDate
+        users {
+          _id
+          firstName
+          lastName
+        }
       }
     }
   }
 `;
 
-export const QUERY_CATEGORY_TASKS = gql`
-  query getCategoryTasks($categoryId: ID!) {
-    categoryTasks(categoryId: $categoryId) {
-      _id
-      categoryName
-      tasks {
-        _id
-        taskName
-        taskDescription
-        dueDate
-      }
-    }
-  }
-`;
+// export const QUERY_CATEGORY_TASKS = gql`
+//   query getCategoryTasks($categoryId: ID!) {
+//     categoryTasks(categoryId: $categoryId) {
+//       _id
+//       categoryName
+//       tasks {
+//         _id
+//         taskName
+//         taskDescription
+//         dueDate
+//       }
+//     }
+//   }
+// `;
 
 export const QUERY_SINGLE_TASK = gql`
   query getSingleTask($taskId: ID!) {
@@ -117,7 +114,6 @@ export const QUERY_SINGLE_TASK = gql`
         _id
         firstName
         lastName
-        email
       }
       comments {
         _id
