5ec60639992e577500fb9ff3fcedd697b2b63ba6
Jack <jebulger1003@gmail.com>
2023-08-29 20:37:35 -0400

    More styling to feature/dashboard

--- client/src/components/UserGroups.jsx
+++ client/src/components/UserGroups.jsx
@@ -54,7 +54,7 @@ function UserGroups() {
       <div className="grid grid-cols-1 gap-10 mt-10 mb-10 place-content-stretch md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 2xl:mt-20 2xl:mb-20">
         {userGroups.map((group) => (
           <Link key={group._id}>
-            <Card className="shadow-2xl hover:bg-gray-200">
+            <Card className="shadow-2xl hover:bg-white bg-slate-300">
               <h5 className="flex items-center justify-center flex-grow text-lg font-bold tracking-tight text-gray-900 md:text-xl xl:text-2xl 2xl:text-3xl dark:text-white">
                 <p>{group.groupName}</p>
               </h5>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,11 +5,11 @@ function Dashboard() {
   const { currentUser } = useCurrentUserContext();
 
   return (
-    <div className="h-screen p-6 bg-gray-50 dark:bg-slate-800">
+    <div className="h-screen p-6 bg-slate-200 dark:bg-slate-800">
       <h1 className="mb-8 text-3xl font-semibold text-center md:text-left md:text-4xl 2xl:text-5xl 2xl:mt-8 dark:text-white">
         Welcome, {currentUser.firstName}!
       </h1>
-      <div className="w-11/12 p-8 mx-auto bg-blue-400 2xl:mt-20 h-fit dark:bg-slate-500">
+      <div className="w-11/12 p-8 mx-auto bg-blue-400 rounded-xl 2xl:mt-20 h-fit dark:bg-slate-500">
         <div>
           <h1 className="text-2xl font-semibold text-center md:text-3xl 2xl:text-4xl dark:text-white">
             Your Groups
