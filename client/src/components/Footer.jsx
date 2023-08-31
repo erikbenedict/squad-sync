bcb33ddb25fffd3aceaf29d3a68b245e7234a94a
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-31 16:39:08 -0400

    fix: footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,11 @@
-    <footer className=" shadow dark:bg-gray-900">
+export default function Footer() {
+  <footer className=" shadow dark:bg-gray-900">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm sm:text-center dark:text-gray-400">© 2023 <a>Squadsync™</a>. All Rights Reserved.</span>
+      <div className="sm:flex sm:items-center sm:justify-between"></div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm sm:text-center dark:text-gray-400">
+        © 2023 <a>Squadsync™</a>. All Rights Reserved.
+      </span>
     </div>
-</footer>
+  </footer>;
+}
