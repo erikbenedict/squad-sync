ddab3da6522b2d19a7e48209b3bcc0c95f1fd117
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-31 16:28:36 -0400

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,9 @@
-function Footer() {
-  return (
-    <footer>Footer</footer>
-  )
-}
-
-export default Footer
+    <footer className=" shadow dark:bg-gray-900">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm sm:text-center dark:text-gray-400">© 2023 <a>Squadsync™</a>. All Rights Reserved.</span>
+    </div>
+</footer>
